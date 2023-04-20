82641bec2b0f312a5718577b4af6b1965295e027
Chí Hiếu <hieumeo171@gmail.com>
2023-04-20 09:29:38 +0700

    add gltf loader

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
+// SCENE + CAMERA + RENDERER
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
@@ -7,13 +9,27 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+// LOAD MATERIALS
+const loader = new GLTFLoader();
+
+loader.load( 'D:/A41174/ThreeJS/models/materials/a41174.glb', 
+	function ( gltf ) {
+		scene.add( gltf.scene );
+	}, 
+	undefined, function ( error ) {
+		console.error( error );
+	}
+);
+
+// OBJECT
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+// scene.add( cube );
 
 camera.position.z = 5;
 
+// ANIM
 function animate() {
 	requestAnimationFrame( animate );
 
@@ -22,5 +38,4 @@ function animate() {
 
 	renderer.render( scene, camera );
 }
-
-animate();
+// animate();
